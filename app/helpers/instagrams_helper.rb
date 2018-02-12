bb89c46505fe37013ac04339f726901f5cdb4182
ryota <ryota-k@nttpc.co.jp>
2018-02-12 05:58:40 +0000

    heroku

--- app/helpers/instagrams_helper.rb
+++ app/helpers/instagrams_helper.rb
@@ -1,2 +1,9 @@
 module InstagramsHelper
+    def new_or_edit_checkbox
+      if action_name == 'new'
+         false
+      elsif action_name == 'edit'
+         @taste.split(/\s*,\s*/).include?(taste)
+      end
+    end
 end
