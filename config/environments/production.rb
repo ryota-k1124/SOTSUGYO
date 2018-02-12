b8d2201a382e04326bead04dfc84006f87118108
ryota <ryota-k@nttpc.co.jp>
2018-02-12 00:52:52 +0000

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: '54.238.110.131' }
+  config.action_mailer.default_url_options = { host: 'polar-shore-28541.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
